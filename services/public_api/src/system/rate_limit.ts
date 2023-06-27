aeb10879278ec3a2ac01c99e006034c1974de53c
Thanh Tran <>
2023-06-27 17:13:11 +0700

    add rate limit

---
+++ services/public_api/src/system/rate_limit.ts
@@ -0,0 +1,38 @@
+import { NextFunction, Request, Response } from "express";
+
+import { StartRedis } from './redis';
+
+export interface RateLimitRule {
+    endpoint: string,
+    rate_limit: {
+        time: number,
+        limit: number
+    }
+}
+
+export class rateLimitSchema {
+    constructor() { }
+  
+    limit(rule: RateLimitRule) {
+        const { endpoint, rate_limit } = rule;
+        return async (req: Request, res: Response, next: NextFunction) => {
+            const ip = req.ip;
+            const redis_id = `${endpoint}_${ip}`;
+
+            const redis_client = await StartRedis();
+            const request = await redis_client.incr(redis_id);
+
+            if (request === 1) {
+                await redis_client.expire(redis_id, rate_limit.time);
+            }
+
+            if (request > rate_limit.limit) {
+                return res.status(429).send({
+                    message: 'to much requests'
+                });
+            }
+
+            next();
+        };
+    }
+}
